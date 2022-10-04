5daac00d1183be5b9cbf6d4cb4853c49b4c4b57c
ruin350 <kkr583@naver.com>
2022-10-04 16:03:28 +0900

    4시

---
+++ src/main/java/com/stussy/stussyclone20220930kkr/controller/AccountController.java
@@ -0,0 +1,20 @@
+package com.stussy.stussyclone20220930kkr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AccountController {
+
+
+    @GetMapping("/account/login")
+    public String login(){
+        return "account/login";
+    }
+
+    @GetMapping("/account/register")
+    public String register(){
+        return "account/register";
+    }
+}
